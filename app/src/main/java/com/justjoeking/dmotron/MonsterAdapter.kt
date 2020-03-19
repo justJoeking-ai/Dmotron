f83c0bc6cf1f9f2bebdb8df2d5a2b9fd829435c3
joeking <joe@justjoeking.com>
2020-03-19 00:01:23 -0600

    Try this again

--- app/src/main/java/com/justjoeking/dmotron/MonsterAdapter.kt
+++ app/src/main/java/com/justjoeking/dmotron/MonsterAdapter.kt
@@ -1,12 +1,18 @@
 package com.justjoeking.dmotron
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
-class MonsterAdapter() :
+
+public class MonsterAdapter() :
     RecyclerView.Adapter<MonsterAdapter.MyViewHolder>() {
 
     public var myDataset: ArrayList<MonsterListing> = ArrayList()
@@ -25,17 +31,25 @@ class MonsterAdapter() :
         // create a new view
         val layout = LayoutInflater.from(parent.context)
             .inflate(R.layout.monster_layout, parent, false) as LinearLayout
-
         // set the view's size, margins, paddings and layout parameters
+
         return MyViewHolder(layout)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder (holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.linearLayout.findViewById<TextView>(R.id.monster_name).text =
-            myDataset[position].name
+        holder.linearLayout.findViewById<TextView>(R.id.monster_name).text = myDataset[position].name
+        holder.linearLayout.setOnClickListener(View.OnClickListener { view ->
+            val monsterId = myDataset[position].name
+            val mIntent =  (Intent (holder.linearLayout.context, MonsterCardActivity::class.java));
+            val mBundle = Bundle()
+            mBundle.putString("name", monsterId);
+            mIntent.putExtras(mBundle)
+            startActivity(holder.linearLayout.context, mIntent, mBundle)
+
+        })
 
     }
 
